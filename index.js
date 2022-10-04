4ad9e25e69c3f68aac0c8d669c99ee2f7b94909f
charaf marghin <developermarghin@gmail.com>
2022-10-04 09:23:52 +0100

    sync db and init routes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const ejsLayouts = require("express-ejs-layouts");
 const dotenv = require("dotenv");
+const db = require("./models/");
+const RatingRoutes = require("./routes/RatingRoute");
+const BlogRoutes = require("./routes/BlogRoute");
 
 const app = express();
 dotenv.config();
+app.use(express.json());
 
 // ejs as a default template engine
 app.set("view engine", "ejs");
@@ -12,6 +16,20 @@ app.set("views", __dirname + "/views");
 app.use(ejsLayouts);
 // static files are stored on public filder
 app.use(express.static(__dirname + "/public"));
+
+// conect to db
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log("error connecting to db");
+  });
+
+// routes
+app.use("/rates", RatingRoutes);
+app.use("/blog", BlogRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
